311dea87d4906433dcf17535129f780aba22f430
Jever Osmanova <amidala274@gmail.com>
2021-01-17 00:29:16 +0300

    feat: add service

---
+++ src/app/app.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+  getListOfShifts() {
+    return this.http.get();
+  }
+}
