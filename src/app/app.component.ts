7fd14a83c8f589f9bdf74018e61d8ba7d9b945f1
Jever Osmanova <amidala274@gmail.com>
2021-01-26 00:43:09 +0300

    feat: change table for new data structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { AppService } from './app.service';
+import { ShiftFormDialog } from './shift-form.component';
+import { Shift } from './shift.interface';
+import * as moment from 'moment'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  
+  public displayedColumns: string[] = ['fullName', 'start', 'end', 'craneCount', 'cranes', 'actions'];
+  public dataSource: MatTableDataSource<any>;
 
+  constructor(
+    private appService: AppService,
+    public dialog: MatDialog, 
+    private _snackBar: MatSnackBar
+    ) { }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngOnInit(): void {
+    this.getShifts();
+  }
+
+  ngOnDestroy(): void {
+   
+  }
+
+  getShifts(): void {
+    this.appService.getListOfShifts().subscribe(data => {
+      this.dataSource = new MatTableDataSource<Shift>(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  formatDate(date) {
+    return moment(date).format('DD.MM.YY H-mm');
+  }
+
+  getTotalLoaded(cranes) {
+    let result = 0;
+
+    for (const crane of cranes) {
+      for (const { loaded } of crane) {
+        result += +loaded;
+      }
+    }
+
+    return result || null;
+  }
+
+  public addShift(): void {
+    const dialogRef = this.openDialog({title: 'Добавление',  isChange: false}, ShiftFormDialog);
+    this.afterClosed(dialogRef);
+  }
+
+  public editShift(shift: Shift) {
+    console.log("this.dataSource");
+    const dialogRef = this.openDialog({shift, title: 'Редактирование', isChange: true}, ShiftFormDialog);
+    this.afterClosed(dialogRef);
+  }
+
+  public deleteShift(id: number) {
+    this.appService.deleteShift(id).subscribe(() => {
+      this.getShifts();
+    });
+  }
+
+  private openDialog(data, dialog) {
+    return this.dialog.open(dialog, {
+      width: '650px',
+      data: data
+    });
+  }
+
+  private afterClosed(dialogRef) {
+    dialogRef.afterClosed()
+    .subscribe(() => {
+      this.getShifts();
+    });
+  }
+
+  private errorMessage(message): void {
+    this._snackBar.open(message, 'Закрыть', {
+      duration: 2000,
+      verticalPosition: 'top',
+    });
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,52 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { ShiftFormDialog } from './shift-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShiftFormDialog
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
 
     MatTableModule,
     MatPaginatorModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   exports: [
     MatTableModule,
     MatPaginatorModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
